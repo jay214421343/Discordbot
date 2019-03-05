aed00f4407696046cd4b8aa3999200f3462caad1
GameFuzzy <awsmajed7@gmail.com>
2019-03-05 22:02:10 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -117,7 +117,7 @@ async def on_raw_reaction_add(payload):  # Will be dispatched every time a user 
 		return
 	
 	reactionMessage = client.get_channel(payload.channel_id).get_message(payload.message_id)
-	await client.remove_reaction(payload.emoji, member)
+	await reactionMessage.remove_reaction(payload.emoji, member)
 	await member.add_roles(role, reason='Invited to clan')  # Finally add the role to the member
 	print("Added role")
 client.run(os.environ['discordToken'])
