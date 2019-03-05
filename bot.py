d2a593a72d47fe83220fc19bcaa5331eda25deee
GameFuzzy <30367459+GameFuzzy@users.noreply.github.com>
2019-03-05 12:20:44 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -101,7 +101,7 @@ async def on_raw_reaction_add(payload):  # Will be dispatched every time a user 
 	if str(payload.emoji) == str(os.environ['emojiIDMember']):  # payload.emoji is a PartialEmoji. You have different possibilities to check for a proper reaction
 		print(os.environ['emojiIDMember'])
 		print(payload.emoji)
-		role = discord.Object(id=str(os.environ['roleIDMember'])) # You also need the role
+		role = client.get_channel(str(os.environ['roleIDMember'])) # You also need the role
 		messageChannel = discord.Object(id=str(os.environ['channelID']))
 		await messageChannel.send(os.environ['memberJoinMessage'])
 	# Gotta do same thing for friends
