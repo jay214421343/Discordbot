c9c718de6dab7c46e406607c67a38a71c3481c49
GameFuzzy <awsmajed7@gmail.com>
2019-03-06 20:19:03 +0100

    Why do I do this to myself?

--- bot.py
+++ bot.py
@@ -6,8 +6,6 @@ import psycopg2
 
 DATABASE_URL = os.environ['DATABASE_URL']
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
 logging.basicConfig(level=logging.INFO)
 
 client = discord.Client()
@@ -18,11 +16,12 @@ async def on_ready():
 	print(client.user.id)
 	print('------')
 	
+	mentionMessages = []
 	botActivity = discord.Activity(name=os.environ['activityName'],type=discord.ActivityType.watching)
 	await client.change_presence(activity = botActivity)
 
 	
-#I DON'T KNOW
+# I DON'T KNOW
 
 @client.event
 async def on_message(message):
@@ -76,7 +75,7 @@ async def on_message(message):
 				epicLikeliness = "are epic"
 			await message.channel.send(f'{mention} has now been called epic {epiccount} {timeform}\n They {epicLikeliness}')
 
-#REACTION ROLES
+# NEW MEMBER REACTION ROLES
 
 @client.event
 async def on_raw_reaction_add(payload):  # Will be dispatched every time a user adds a reaction to a message the bot can see
@@ -90,21 +89,55 @@ async def on_raw_reaction_add(payload):  # Will be dispatched every time a user 
 		
 	# At this point, you'd have to implement something like a check to ensure the reaction was added to the proper message
 	# Either by hardcoding the ID or using a better way like storing the message id.
-	if payload.message_id != int(os.environ['messageID']):
-		print(os.environ['messageID'])
-		print(payload.message_id)
-		print("Wrong messageID")
-		return
-	print("Reaction added to message")
 	
 	guild = client.get_guild(payload.guild_id)  # You need the guild to get the member who reacted
 	member = guild.get_member(payload.user_id)  # Now you have the key part, the member who should receive the role
 	
+	if payload.message_id != int(os.environ['messageID']):
+		print(os.environ['messageID'])
+		print(payload.message_id)
+		print("Wrong messageID")
+		
+		conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+		
+		try:
+			cur = conn.cursor()
+			#Add SQL injection protection
+			cur.execute("SELECT * FROM mentionMessageTable WHERE id=" + payload.message_id)
+			mentionMessage = cur.fetchone()
+			if mentionMessage[0] == payload.message_id:
+				#Add SQL injection protection
+				cur.execute("DELETE FROM mentionMessageTable WHERE id=" + payload.message_id)
+				welcomeChannel = client.get_channel(int(os.environ['welcomeChannelID']))
+				await welcomeChannel.send(os.environ['inviterPingMessage'] + " dabdabdab" + member.nick)
+		cur.close()
+		except (Exception, psycopg2.DatabaseError) as error:
+			print(error)
+		finally:
+			if conn is not None:
+				conn.close()
+		return
+		
+	print("Reaction added to message")
+	
 	if str(payload.emoji) == str(os.environ['emojiIDMember']):  # payload.emoji is a PartialEmoji. You have different possibilities to check for a proper reaction
 		print("Emoji matches")
 		role = guild.get_role(int(os.environ['roleIDMember'])) # You also need the role
-		messageChannel = client.get_channel(int(os.environ['channelID']))
-		mentionMessage = await messageChannel.send(os.environ['memberJoinMessage'] + " please invite " + member.nick)
+		messageChannel = client.get_channel(int(os.environ['inviterChannelID']))
+		mentionMessageDab = await messageChannel.send(os.environ['inviterPingMessage'] + " please invite " + member.nick)
+	try:
+		conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+		cur = conn.cursor()
+		#Add SQL injection protection
+		cur.execute(f"INSERT INTO mentionMessageTable VALUES (" + mentionMessageDab.id + ", " + payload.user_id + ")")
+		conn.commit()
+		cur.close()
+	except (Exception, psycopg2.DatabaseError) as error:
+		print(error)
+	finally:
+		if conn is not None:
+			conn.close()
+ 
 		print("Sent message")
 	# Gotta do same thing for friends
 	elif str(payload.emoji) == str(os.environ['emojiIDFriend']):
