1f70878a23ee0e34df352cae5d04c82d1cf2c243
GameFuzzy <awsmajed7@gmail.com>
2019-03-09 14:05:53 +0100

    Removed test bot

--- bot.py
+++ bot.py
@@ -22,59 +22,6 @@ async def on_ready():
 	#await client.user.edit(username="Bot Mcbotface") Can be used to change the bot username.
 
 	
-# I DON'T KNOW
-
-@client.event
-async def on_message(message):
-	if message.author == client.user:
-		return
-	print(f"Message sent by {message.author}")
-	if message.content.startswith("<@") and len(message.content.split()) == 3:
-		print("Mention found")
-		print(message.content.split())
-		print(len(message.content.split()))
-		mention,wordis,epicword = message.content.split()
-		if wordis == "is" and epicword == "epic":
-			print("is epic")
-			with open("botdatabase","r") as db:
-				dbLines = db.readlines()
-			namefound = False
-			dbNewLines = []
-			for line in dbLines:
-				userElements = line.split('=')
-				if userElements[0] == mention:
-					print("ID found")
-					timeform = "times"
-					userElements[1] = str(int(userElements[1]) + 1)
-					print("Epiccounter = " + userElements[1])
-					epiccount = userElements[1] 
-					namefound = True
-					print("Modified line:" + userElements[0] + "=" + userElements[1])
-					if len(userElements) == 2:
-						dbNewLines.append(f"{userElements[0]}={userElements[1]}\n")
-					break
-				print(userElements[0] + "=" + userElements[1] + "not matching")
-				if len(userElements) == 2:
-					dbNewLines.append(f"{userElements[0]}={userElements[1]}")
-			if namefound == True:
-				with open("botdatabase","w") as dbw:
-					dbw.writelines(dbNewLines)
-			else:
-				with open("botdatabase","a") as dba:
-					dba.write(f"{mention}=1" + "\n")
-				epiccount = 1
-				timeform = "time"
-			if int(epiccount) < 5:
-				epicLikeliness = "are probably not epic"
-			elif int(epiccount) < 10:
-				epicLikeliness = "might be epic"
-			elif int(epiccount) < 20:
-				epicLikeliness = "are likely epic"
-			elif int(epiccount) < 30:
-				epicLikeliness = "are very likely epic"
-			elif int(epiccount) < 50:
-				epicLikeliness = "are epic"
-			await message.channel.send(f'{mention} has now been called epic {epiccount} {timeform}\n They {epicLikeliness}')
 
 # NEW MEMBER REACTION ROLES
 
