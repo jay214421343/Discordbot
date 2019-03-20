189fad329313fd2c3452cee367e04c282bb5d01a
GameFuzzy <awsmajed7@gmail.com>
2019-03-20 19:30:02 +0100

    Yes

--- bot.py
+++ bot.py
@@ -51,7 +51,7 @@ def nickOrName(dabbermember):
 
 
 async def deleteErrorMessage(dabErrorMessage):
-    await asyncio.sleep(5)
+    await asyncio.sleep(20)
     await dabErrorMessage.delete()
     client.deleteErrorMessage_task.cancel()
 
